6e5c0e12be350f478dac34a37beead1ee05a16f5
pkuwwt <wwthunan@gmail.com>
2018-09-13 23:11:20 +0800

    Create PathTree.js

---
+++ js/app/PathTree.js
@@ -0,0 +1,11 @@
+
+define(['vue'], function(Vue) {
+  var comp = Vue.extend({
+    template: ``,
+    data () {
+      return {};
+    },
+  });
+  Vue.component('PathTree', comp);
+  return Vue.component('PathTree');
+});
