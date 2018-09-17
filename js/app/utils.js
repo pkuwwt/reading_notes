c101b467f7d394993f5a619d1700ae66531efdd2
pkuwwt <wwthunan@gmail.com>
2018-09-18 01:52:42 +0800

    Update utils.js

--- js/app/utils.js
+++ js/app/utils.js
@@ -20,6 +20,41 @@ define(['axios.min'], function(axios) {
 			.then(resp => resp.data.files)
 			.then(files => parse_files(files));
 	}
+	
+	function url_origin(url) {
+		var protocol = "http";
+		var protocols = ['http','https'];
+		for(var p of protocols) {
+			var s = p + '://';
+			if(url.startsWith(s)) {
+				url = url.slice(s.length);
+				protocol = p;
+				break;
+			}
+		}
+		url = url.split('/')[0];
+		return protocol + '://' + url;
+	}
+	
+	function is_same_origin(url) {
+		return url_origin(url)===window.location.origin;
+	}
+	
+	function get_resource(url) {
+		if(is_same_origin(url)) 
+			return get_resource_same_origin(url);
+		else 
+			return get_resource_different_origin(url);
+	}
+	
+	function get_resource_same_origin(url) {
+		axios.get(url).then(resp => resp.data);
+	}
+	
+	function get_resource_different_origin(url) {
+		var iframe = document.createElement('iframe');
+		//TODO
+	}
 
 	return {parse_json};
 });
