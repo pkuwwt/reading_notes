399d2218b801c120b2638523e2b544484d5aad05
pkuwwt <wwthunan@gmail.com>
2018-10-08 03:21:36 +0800

    Update MarkdownViewer.js

--- js/app/views/MarkdownViewer.js
+++ js/app/views/MarkdownViewer.js
@@ -3,7 +3,6 @@ define(['vue', 'libs/vue-markdown'], function(Vue,VueMarkdown) {
   var name = 'MarkdownViewer';
   var comp = Vue.extend({
     name,
-    props: ['markdown'],
     template: `<vue-markdown><slot>$E=mc^2$</slot></vue-markdown>`,
     data () {
       return {};
